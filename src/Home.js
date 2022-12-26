f9a05e35814d7956c7c76ad53eeff1e3b3fdebcf
Divakar <divakar2163@gmail.com>
2022-12-26 13:23:06 +0530

    setTimeout and fetching

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,33 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
-      const handleDelete=(id)=>{
-        // remove the id from blogs copy
-        const newblogs=blogs.filter(blog=> blog.id !== id);
-        // set this newblogs from usestate hook
-        setBlogs(newblogs);
-      }
-      useEffect(()=>{
-        console.log("usefecct is running");
-        
-      },[])
-    return (
-        <div className="home">
-        {/* props as => variable=data_value */}
-        <BlogList blogs={blogs} title="Our Blogs" handleDelete={handleDelete}/>
-        
-        </div>
-    );
-}
- 
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading,setIsLoading]=useState(true);
+  useEffect(() => {
+    setTimeout(()=>{
+      // returns a promise object
+    fetch("http://localhost:8000/blogs")
+    .then((res) => {
+      // convert that response from above to a json
+      return res.json();
+    })
+    .then((data) => {
+      //taking the above data as json
+      // console.log(data);
+      //and also mark loading flag as false
+      setIsLoading(false)
+      setBlogs(data)
+    });
+    },1000)
+  }, []);
+  return (
+    <div className="home">
+      {/* props as => variable=data_value */}
+
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="Our Blogs" />}
+    </div>
+  );
+};
+
 export default Home;
