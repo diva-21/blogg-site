c095808f710f592f2b9723578268fecfd5a3c076
Divakar <divakar2163@gmail.com>
2022-12-26 16:21:36 +0530

    route params

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const BlogList = ({blogs,title}) => {
     // props is an object, so destructeit
     // const blogs=props.blogs
@@ -9,9 +10,10 @@ const BlogList = ({blogs,title}) => {
         <h2>{title}</h2>
             {blogs.map((blog)=>(
             <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-            
+            <Link to={`/blogs/${blog.id}`}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </Link>
             </div>
         ))}    
         </div>
